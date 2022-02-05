bcdf6c9d45866e24d6c657519e3e57f5fab8c814
BatoolMM <batool@liverpool.ac.uk>
2022-02-05 17:15:17 +0300

    fix link and typo

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'batool@liverpool.ac.uk';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,7 +17,7 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
